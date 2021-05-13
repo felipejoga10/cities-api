2675e4e93b4b5bd6ac76ce01608e0dc6b4f74937
felipejoga10 <bavaresco3@yahoo.com.br>
2021-05-13 10:08:26 -0300

    Building a REST API for consulting cities of Brazil

--- personappi/src/main/java/dio/personappi/controller/PersonController.java
+++ personappi/src/main/java/dio/personappi/controller/PersonController.java
@@ -1,14 +1,36 @@
 package dio.personappi.controller;
 
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import dio.personappi.dto.request.PersonDTO;
+import dio.personappi.dto.response.MessageResponseDTO;
+import dio.personappi.service.PersonService;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping String getBook() {
-        return "API Test.";
+    private PersonService personService;
+
+    @Autowired(required = true)
+    public PersonController(PersonService personService) {
+        super();
+        this.personService = personService;
     }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
+    }
+
 }
